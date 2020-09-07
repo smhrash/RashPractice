2654fb85fb79878c9b7a94bfd184726b331135ad
smhrash <smhrash@gmail.com>
2020-09-07 08:33:38 -0400

    update

--- src/problem_solving/Pyramid.java
+++ src/problem_solving/Pyramid.java
@@ -1,12 +1,18 @@
 package problem_solving;
 
+import java.util.Scanner;
+
 public class Pyramid {
 
+
     public static void pyramidPattern() {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Please enter the height of Pyramid");
+        int numOfRow=scan.nextInt();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < numOfRow; i++) {
 
-            for (int j = 0; j < 5-i; j++) {
+            for (int j = 0; j < numOfRow-i; j++) {
                 System.out.print(" ");
             }
             for (int k = 0; k <= i; k++) {
@@ -14,7 +20,7 @@ public class Pyramid {
             }
             System.out.println();
 
-
+            
         }
     }
 
